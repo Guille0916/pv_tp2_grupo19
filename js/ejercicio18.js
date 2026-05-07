41226e81df2fabe1423b1b329d6231a0b978628f
francozxy <thedarck777@gmail.com>
2026-05-06 21:55:54 -0300

    ahregando ejercicio18

---
+++ js/ejercicio18.js
@@ -0,0 +1,15 @@
+function calcularMayor(numero1,numero2){
+
+    if(numero1>numero2){
+
+        return numero1;
+    }else if(numero1!=numero2){
+        return numero2;
+    }
+ if(numero1==numero2)
+{
+    return "los numeros son iguales";
+}
+
+}
+console.log(calcularMayor(43,46));
